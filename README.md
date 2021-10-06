d8f98bf98cde23a16661862dc695582a6e9143b2
Stewart Lynch <slynch@createchsol.com>
2021-10-06 16:59:48 -0700

    Removed Extra file and added some content to the ReadMe to give credit to the author

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # SegmentedPicker
 
-A description of this package.
+ > NOTE:  This sample code is primarly taken from the article by Frank Jia in the article
+ published at https://betterprogramming.pub/custom-ios-segmented-control-with-swiftui-473b386d0b51
+ ### Build a Custom iOS Segmented Control With SwiftUI
+ I have made several minor modifications but for the most part,
+ I make no claim to the code, I am using it only as an example for my tutorial.
+ 
+ 
--- Sources/SegmentedPicker/SegmentedPicker.swift
+++ Sources/SegmentedPicker/SegmentedPicker.swift
@@ -7,14 +7,6 @@
 
 import SwiftUI
 
-/*
- NOTE:  This sample code is primarly taken from the article by Frank Jia in the article
- published at https://betterprogramming.pub/custom-ios-segmented-control-with-swiftui-473b386d0b51
- Build a Custom iOS Segmented Control With SwiftUI
- I have made several minor modifications but for the most part,
- I make no claim to the code, I am using it only as an example for my tutorial.
- */
-
 public struct SegmentedPicker: View {
     private static let ActiveSegmentColor: Color = Color(.tertiarySystemBackground)
     private static let BackgroundColor: Color = Color(.secondarySystemBackground)
